335dd39cb187daa1332a7794fd58548d70602b2b
maj357 <83345668+maj357@users.noreply.github.com>
2021-06-26 16:59:49 +0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -6,16 +6,10 @@ $(function () {
     infinite: false,
   });
 
-
-  // $('.questions__item-title').on('click', function () {
-  //   $('.questions__item').removeClass('questions__item--active');
-  //   $(this).parent().addClass('questions__item--active');
-  // });
-
   $('.questions__item-title').click(function () {
     $(this).toggleClass('in').next().slideToggle();
     $('.questions__item-title').not(this).removeClass('in').next().slideUp();
- });
+  });
 
   $('#fullpage').fullpage({
     autoScrolling: true,
@@ -24,6 +18,7 @@ $(function () {
     scrollOverflow: true,
     menu: '#header__nav',
     anchors: ['top', 'products', 'benefits', 'specification', 'questions', 'contacts'],
+    responsiveWidth: 420,
   });
 
   $('.menu__btn').on('click', function () {
@@ -35,6 +30,5 @@ $(function () {
     $('.menu__btn').removeClass('menu__btn--active');
     $('.menu__list').removeClass('menu__list--active');
   });
-
 
 });
